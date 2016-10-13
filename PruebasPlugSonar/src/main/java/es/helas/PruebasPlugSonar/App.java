1d385a085efe773e0081acc271b5f1288b787c01
Hector Arranz <hector_lastras@hotmail.com>
2016-10-13 20:46:00 +0200

    Novedades para meter un bug a analizar en el pull-request

--- PruebasPlugSonar/src/main/java/es/helas/PruebasPlugSonar/App.java
+++ PruebasPlugSonar/src/main/java/es/helas/PruebasPlugSonar/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Prueba desde GitHub" );
         System.out.println( "Prueba desde GitHub2 en Fork" );
+        int a = 5;
+        if (a==5) System.out.println("a es 5");
     }
 }
